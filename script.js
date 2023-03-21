185e2db498f03c6575ba17a2f3bc115ef9f9f836
Guilherme Shibuya <73371141+guilhermeshibuya@users.noreply.github.com>
2023-03-20 21:01:20 -0300

    Home mobile: done, Navbar mobile: done, About me mobile: started, Skills mobile: started

--- script.js
+++ script.js
@@ -13,3 +13,24 @@ if (navClose) {
         navMenu.classList.remove("show-menu");
     });
 }
+
+// Hide navbar mobile
+const navLinks = document.querySelectorAll(".nav__link");
+
+const navHide = () => {
+    const navMenu = document.getElementById("nav-menu");
+
+    navMenu.classList.remove("show-menu");
+}
+
+navLinks.forEach(link => link.addEventListener("click", navHide));
+
+// Scroll header mobile
+const header = document.getElementById("header");
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 15) {
+        header.classList.add("nav-scroll");
+    } else {
+        header.classList.remove("nav-scroll");
+    }
+});
