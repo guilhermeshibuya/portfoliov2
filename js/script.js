575d9bdba32425eff5724fe96fddd31cdc2e71f8
Guilherme Shibuya <73371141+guilhermeshibuya@users.noreply.github.com>
2023-03-24 15:35:36 -0300

    added toggle button for dark mode

--- js/script.js
+++ js/script.js
@@ -35,3 +35,40 @@ window.addEventListener("scroll", () => {
         header.classList.remove("nav-scroll");
     }
 });
+
+// DARK MODE
+
+//button
+const btnDarkMode = document.getElementById("btn-dark-mode");
+const btnDarkModeContainer = document.getElementById("btn-container");
+const ball = document.getElementById("ball");
+const sun = document.getElementById("sun");
+const moon = document.getElementById("moon");
+
+//body
+const body = document.getElementById("body");
+
+//nav
+const navLogo = document.getElementById("nav-logo");
+
+//section title
+const sectionTitles = document.querySelectorAll(".section__title");
+
+btnDarkMode.addEventListener("click", () => {
+    btnDarkModeContainer.classList.toggle("lightiest-background");
+    ball.classList.toggle("dark-background");
+    sun.classList.toggle("dark-font");
+    moon.classList.toggle("dark-font");
+
+    body.classList.toggle("dark-background");
+    body.classList.toggle("lighter-font");
+
+    sectionTitles.forEach(title => title.classList.toggle("lightiest-font"));
+
+    header.classList.toggle("dark-light-background");
+    navMenu.classList.toggle("dark-light-background");
+    navToggle.classList.toggle("lighter-font");
+    navLogo.classList.toggle("lighter-font");
+    navLinks.forEach(link => link.classList.toggle("lighter-font"));
+
+});
